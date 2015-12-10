31ad70c106280a572b3ad114eab4ad8ff4fdd515
Iris Molinari <molinari@acsinfo.ch>
2015-12-10 13:54:25 +0100

    add materialize
    Use external link for material-design-icons. More details:
    https://github.com/Dogfalo/materialize/issues/1079

---
+++ app/assets/javascripts/materialize.js
@@ -0,0 +1,31 @@
+// Materialize js come with a concat order.
+// Take a look at https://github.com/Dogfalo/materialize/blob/master/Gruntfile.js
+//
+//= require materialize_components/jquery.easing.1.3
+//= require materialize_components/animation
+//= require materialize_components/velocity.min
+//= require materialize_components/hammer.min
+//= require materialize_components/jquery.hammer
+//= require materialize_components/global
+//= require materialize_components/collapsible
+//= require materialize_components/dropdown
+//= require materialize_components/leanModal
+//= require materialize_components/materialbox
+//= require materialize_components/parallax
+//= require materialize_components/tabs
+//= require materialize_components/tooltip
+//= require materialize_components/waves
+//= require materialize_components/toasts
+//= require materialize_components/sideNav
+//= require materialize_components/scrollspy
+//= require materialize_components/forms
+//= require materialize_components/slider
+//= require materialize_components/cards
+//= require materialize_components/chips
+//= require materialize_components/pushpin
+//= require materialize_components/buttons
+//= require materialize_components/transitions
+//= require materialize_components/scrollFire
+//= require materialize_components/date_picker/picker
+//= require materialize_components/date_picker/picker.date
+//= require materialize_components/character_counter
