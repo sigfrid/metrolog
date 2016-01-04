856357e6adb172a85b482111cd5f6f9b797bd871
Sigfrid Dusci <sigfrid.dusci@swelement.com>
2016-01-04 16:05:03 +0100

    add materialized typeahead

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,9 +14,47 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require materialize
+//= require typeahead
 //= require_tree .
 
   $(document).ready(function(){
     $('.modal-trigger').leanModal();
+
+    var equipment_tokens = new Bloodhound({
+      datumTokenizer: Bloodhound.tokenizers.whitespace,
+      queryTokenizer: Bloodhound.tokenizers.whitespace,
+      local: equipments
+    });
+
+    var equipment_typeahead = $('#equipment_input').typeahead(
+    {
+      hint: false,
+      highlight: true,
+      minLength: 1,
+      classNames: {
+          menu: 'typeahead-content',
+          highlight: 'typeahead-highlight',
+          cursor: 'typeahead-cursor',
+          dataset: 'typeahead-dataset',
+          suggestion: 'typeahead-suggestion'
+      }
+    },
+    {
+      name: 'equipments',
+      source: equipment_tokens,
+      templates: {
+        empty: [
+          '<div class="typeahead-notfound">Not Found!</div>'
+        ]
+      }
+    });
+
+    // Litle hack for Materializecss bug with label.
+    equipment_typeahead.unwrap();
   });
 
+var equipments = [  'Autoclave Fedegari FOW4 - PV19',
+                    'Thermistor Pt-100 TE1',
+                    'Thermistor Pt-100 TE2',
+                    'Thermistor Pt-100 TE3',
+                    'Thermistor Pt-100 TE4' ];
