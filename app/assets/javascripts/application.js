839351ae15928948e070aa42bc98a97d22354f5e
Sigfrid Dusci <sigfrid.dusci@swelement.com>
2016-01-06 15:52:55 +0100

    update form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,7 +26,13 @@
       local: equipments
     });
 
-    var equipment_typeahead = $('#equipment_input').typeahead(
+    var activity_tokens = new Bloodhound({
+      datumTokenizer: Bloodhound.tokenizers.whitespace,
+      queryTokenizer: Bloodhound.tokenizers.whitespace,
+      local: activities
+    });
+
+    var equipment_typeahead = $('.eqtypeahead').typeahead(
     {
       hint: false,
       highlight: true,
@@ -51,10 +57,75 @@
 
     // Litle hack for Materializecss bug with label.
     equipment_typeahead.unwrap();
+
+    var activity_typeahead = $('.actypeahead').typeahead(
+    {
+      hint: false,
+      highlight: true,
+      minLength: 1,
+      classNames: {
+          menu: 'typeahead-content',
+          highlight: 'typeahead-highlight',
+          cursor: 'typeahead-cursor',
+          dataset: 'typeahead-dataset',
+          suggestion: 'typeahead-suggestion'
+      }
+    },
+    {
+      name: 'activities',
+      source: activity_tokens,
+      templates: {
+        empty: [
+          '<div class="typeahead-notfound">Not Found!</div>'
+        ]
+      }
+    });
+
+    activity_typeahead.unwrap();
+
   });
 
 var equipments = [  'Autoclave Fedegari FOW4 - PV19',
-                    'Thermistor Pt-100 TE1',
+                    'Heat exchanger ABC HE1',
+                    'Pneumatic valve XXX VP201',
+                    'Pneumatic valve XXX VP202',
+                    'Pneumatic valve XXX VP203',
+                    'Pneumatic valve XXX VP204',
+                    'Pneumatic valve XXX VP205',
+                    'Pneumatic valve XXX VP206',
+                    'Pneumatic valve XXX VP207',
+                    'Pneumatic valve XXX VP208',
+                    'Pneumatic valve XXX VP209',
+                    'Pneumatic valve XXX VP212',
+                    'Recirculation pump P-MAX RP1',
                     'Thermistor Pt-100 TE2',
                     'Thermistor Pt-100 TE3',
-                    'Thermistor Pt-100 TE4' ];
+                    'Thermistor Pt-100 TE4',
+                    'Thermistor Pt-100 TE5',
+                    'Thermistor Pt-100 TE9',
+                    'Thermistor Pt-100 TE10' ];
+
+ var activities = [ '121°C check',
+                    'Calibration',
+                    'Closing doors: limit switch closing and opening doors check',
+                    'Closing doors: seal and alignment photocell check',
+                    'Check mains water filter clogging',
+                    'Check steam filter clogging',
+                    'Check water level indicator',
+                    'Compressed air circuit seal check',
+                    'Compressed air filter replacement',
+                    'External visual inspection',
+                    'Gasket cover seal mains water filter replacement',
+                    'Gasket cover seal steam filter replacement',
+                    'Heat distribution in chamber verification in absence of load',
+                    'Heat penetration in chamber in presence of maximum load 50/100 ml bags',
+                    'Heat penetration in chamber in presence of maximum load 150/200 ml bags',
+                    'Heat penetration twist off in presence of maximum load',
+                    'Heat penetration verification in chamber in presence of minimum load 50/100 ml',
+                    'Heat penetration verification in chamber in presence of minimum load 150/200 ml',
+                    'Pressure leak verification',
+                    'Pressure transducer control',
+                    'Safety valves replacement',
+                    'Seals disassembly and inspection',
+                    'seals leak control',
+                    'Usage verification']
